7b2c692cd75b340d015ab3f32ac0555e76d5cdd0
BocilHacker01 <101524359+BocilHacker01@users.noreply.github.com>
2022-03-14 10:29:50 +0700

    Add files via upload

---
+++ HackBank.sh
@@ -0,0 +1,73 @@
+#!/system/xbin/bash
+
+sleep 1
+   echo "SUBCRIBE DULU TOD.." | lolcat
+      sleep 3
+          xdg-open https://youtube.com/channel/UC5msFOy-RulyPmiF8ka1t8A
+
+sleep 3
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 1
+echo "\033[32;1m==============================================================="
+toilet -f standard -F gay "HACKBANK"
+echo "\033[31;1m==============================================================="
+echo
+echo
+echo "=============================================================="
+echo "[●] Author : Mr_Sadboy01"
+echo "[●] Team   : Cyber Army Indonesia"
+echo "[●] ConTak : 083197689414"
+echo "[●] pesan  : Jangan Di salah gunakan "
+echo "=============================================================="
+echo
+echo
+echo "PILIH NAMA BANK"
+echo
+echo "1) Bank Syariah Mandiri"
+echo "2) Bank BNI syariah"
+echo "3) Bank Bukopin Syariah"
+echo "4) Bank NTB Syariah"
+echo "5) Permata Bank Syariah"
+echo "6) Bank Muamalat"
+echo "7) Bank Mega Syariah"
+echo "8) Bank BJB Syariah"
+echo "9) BRI Syariah"
+echo "10) BTPN Syariah"
+echo "11) Bank Net Syariah"
+echo "12) BCA Syariah"
+echo "13) Panin Dubai Syariah Bank"
+read -p ' ====> ' pill
+sleep 1
+sleep 2
+sleep 3
+echo "Masukkan No Rek Anda"
+read -p ' ====> ' pill
+sleep 1
+sleep 2
+echo "Sebutkan Nominal Minimal Rp.200.000,- Jika Bukan Rp.200.000,- Kaga Bakalan Kekiri>
+read -p ' ====> ' pill
+sleep 1
+sleep 2
+sleep 3
+sleep 4
+sleep 5
+sleep 6
+sleep 7
+sleep 8
+sleep 9
+echo "Transfer Rp 200.000,- sukses"
+echo
+sleep 1
+sleep 2
+
+sleep 1
+sleep 2
+sleep 3
+sleep 4
